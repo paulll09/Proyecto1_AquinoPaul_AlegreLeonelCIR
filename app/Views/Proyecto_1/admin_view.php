b21028246578dfd7e36b101dc2ae4be1097894a2
paulll09 <aquinopaul2002@gmail.com>
2025-06-19 22:57:28 -0300

    modificacion de estilos en el carrito

--- app/Views/Proyecto_1/admin_view.php
+++ app/Views/Proyecto_1/admin_view.php
@@ -1,11 +1,23 @@
 <?= $this->extend('plantillas/base') ?>
 
 <?= $this->section('contenido') ?>
-  <!-- Sección de Carrusel -->
-
-  <!-- Sección de Contacto Rápido -->
-  <?= $this->include('contenido/contactoRapido') ?>
-
-  <!-- Sección de Ubicación -->
-  <?= $this->include('contenido/ubicacion') ?>
+<!-- Seccion Hero - Carrusel -->
+<section class="hero-section">
+  <div class="hero-container">
+    <!-- Imagen Principal -->
+    <div class="hero-background">
+        <img src="<?= base_url('assets/img/logofinal.jpeg') ?>" alt="Centro Informatico Regional" class="hero-image">
+    </div>
+    
+    <!-- Título Principal -->
+    <div class="hero-content">
+      <h1 class="hero-title">
+        <span class="hero-title-line">Un lugar donde conocer los desafios de las nuevas <span class="highlight-text">Tecnologías</span></span>
+      </h1>
+      <div class="hero-description">
+      </div>
+    </div>
+  </div>
+</section>
+  
 <?= $this->endSection() ?>
